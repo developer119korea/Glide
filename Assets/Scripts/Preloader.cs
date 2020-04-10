a6d798ec5cfb6f733f6dc2e4ac8f3eb617dce691
YoungwanCho <developer119korea@gmail.com>
2020-04-10 23:38:51 +0900

    Preloader & Fade

---
+++ Assets/Scripts/Preloader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Preloader : MonoBehaviour
+{
+    private CanvasGroup fadeGroup = null;
+    private float loadTime = 0;
+    private float minimumLogoTime = 3.0f;
+
+    private void Start()
+    {
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        fadeGroup.alpha = 1;
+
+        if (Time.time < minimumLogoTime)
+            loadTime = minimumLogoTime;
+        else
+            loadTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time < minimumLogoTime)
+        {
+            fadeGroup.alpha = 1 - Time.time;
+        }
+
+        if (Time.time > minimumLogoTime && loadTime != 0)
+        {
+            fadeGroup.alpha = Time.time - minimumLogoTime;
+            if (fadeGroup.alpha >= 1)
+            {
+                Debug.Log("Change the scene");
+            }
+        }
+    }
+}
